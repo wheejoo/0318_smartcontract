56bb8d963da6b278ed3cb315907dcc5acf1fe44e
wheejoo <53321214+wheejoo@users.noreply.github.com>
2021-03-18 14:27:44 +0900

    Update test002.sol

--- test002.sol
+++ test002.sol
@@ -1,19 +1,19 @@
 pragma solidity ^0.4.11;
 
 contract Example005 {
-  function s_operation() returns (uint result) {
+  function sum_operation() returns (uint result) {
     result = sum(11,5);
   }
   
-  function m_operation() returns (uint result) {
+  function minus_operation() returns (uint result) {
     result = minus(11,5);
   }
   
-  function mm_operation() returns (uint result) {
+  function multi_operation() returns (uint result) {
     result = multi(11,5);
   }
   
-  function d_operation() returns (uint result) {
+  function div_operation() returns (uint result) {
     result = div(11,5);
   }
 
@@ -31,6 +31,7 @@ contract Example005 {
       uint result = a * b;
       return result;
     }
+    
   function div(uint a, uint b) returns (uint) {
       uint result = a / b;
       return result;
